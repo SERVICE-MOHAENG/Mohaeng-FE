8db76700a3f44c3cf43a30838d8bb68b7eb23ca5
tccmw <jungmwoo6248@gmail.com>
2026-01-13 20:22:40 +0900

    feat(#1) : 좋아요 버튼 구현

--- libs/ui/src/lib/FeedGrid.tsx
+++ libs/ui/src/lib/FeedGrid.tsx
@@ -1,4 +1,8 @@
 import { Link } from 'react-router-dom';
+import { colors, typography } from '@mohang/ui';
+import Heart from '../assets/heart.svg';
+import RedHeart from '../assets/redHeart.svg';
+import { useState } from 'react';
 
 export interface FeedItem {
   id: string;
@@ -16,6 +20,15 @@ export interface FeedGridProps {
 }
 
 export function FeedGrid({ feeds }: FeedGridProps) {
+  const [hearts, setHearts] = useState<Record<string, boolean>>({});
+
+  const handleHeartClick = (id: string) => {
+    setHearts((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
+
   return (
     <>
       <div className="flex flex-col justify-between grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-10 max-w-7xl mx-auto">
@@ -51,27 +64,53 @@ export function FeedGrid({ feeds }: FeedGridProps) {
             <div className="px-1 flex justify-between items-start">
               <div className="flex-1">
                 {/* 작성자 및 날짜 */}
-                <div className="flex items-center gap-1 text-gray-500 text-sm mb-1 font-medium">
+                <div
+                  className="flex items-center gap-1 text-gray-500 mb-2"
+                  style={{
+                    ...typography.body.BodyM,
+                  }}
+                >
                   <span>{feed.author}</span>
                   <span>|</span>
                   <span>{feed.date}</span>
                 </div>
 
                 {/* 제목 */}
-                <h3 className="text-lg font-bold text-gray-900 mb-1 line-clamp-1 group-hover:text-blue-600 transition-colors">
+                <h3
+                  className="text-gray-900 mb-3 line-clamp-1 group-hover:text-blue-600 transition-colors"
+                  style={{
+                    ...typography.title.sTitleM,
+                  }}
+                >
                   {feed.title}
                 </h3>
 
                 {/* 본문 요약 */}
-                <p className="text-gray-400 text-sm line-clamp-2 leading-snug">
-                  {feed.content}
-                </p>
+                <div className="w-2/3">
+                  <p
+                    className="text-gray-400 line-clamp-2 leading-snug"
+                    style={{
+                      ...typography.body.BodyM,
+                    }}
+                  >
+                    {feed.content}
+                  </p>
+                </div>
               </div>
 
               {/* 좋아요 버튼 섹션 */}
               <div className="ml-4 flex flex-col items-center">
                 <button className="p-2 rounded-full hover:bg-gray-50 transition-colors">
-                  {/* <Heart className="w-5 h-5 text-gray-300" /> */}
+                  <div
+                    className="w-12 h-12 flex justify-center items-center rounded-full border border-gray-200"
+                    onClick={() => handleHeartClick(feed.id)}
+                  >
+                    {hearts[feed.id] ? (
+                      <img src={RedHeart} alt="heart" className="w-2/3" />
+                    ) : (
+                      <img src={Heart} alt="heart" className="w-2/3" />
+                    )}
+                  </div>
                 </button>
                 <span className="text-[11px] font-bold text-gray-400 mt-[-4px]">
                   {feed.likes.toLocaleString()}
@@ -84,9 +123,12 @@ export function FeedGrid({ feeds }: FeedGridProps) {
       <div className="flex justify-center">
         <Link
           to={`/trip/`}
-          className="px-7 py-2.5 border-2 border-[#00c7f2] text-[#00c7f2] rounded-full text-sm font-bold hover:bg-[#00c7f2] hover:text-white transition-all"
+          className="px-7 py-2.5 border-2 border-[#00c7f2] text-[#00c7f2] rounded-full hover:bg-[#00c7f2] hover:text-white transition-all"
+          style={{
+            ...typography.body.BodyM,
+          }}
         >
-          바로가기
+          더 보러가기
         </Link>
       </div>
     </>
